5f640313d253bea8111e80243a2ce45e19390437
aymara <aymaragamer14@gmail.com>
2026-01-16 13:40:34 +0000

    Initial commit

---
+++ CanaryFoodAPI/src/main/java/twinsFood/CanaryFoodAPI/CanaryFoodApiApplication.java
@@ -0,0 +1,13 @@
+package twinsFood.CanaryFoodAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanaryFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanaryFoodApiApplication.class, args);
+	}
+
+}
---
+++ CanaryFoodAPI/src/test/java/twinsFood/CanaryFoodAPI/CanaryFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package twinsFood.CanaryFoodAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanaryFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
