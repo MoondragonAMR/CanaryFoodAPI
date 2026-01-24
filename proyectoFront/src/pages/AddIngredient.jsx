2b420ba1ddd71feb4c477422328a525f2f35f0a4
nayar <nayaragamer14@gmail.com>
2026-01-24 12:06:54 +0000

    Añadir ingrediente

---
+++ proyectoFront/src/pages/AddIngredient.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import IngredientForm from '../components/RecipeForm';
+import { createRecipe } from '../services/alumnosService';
+
+export default function AnadirIngrediente() {
+  const [form, setForm] = useState({ name: '', type: '' , vegetarian: '', vegan: '',
+    sugar: '', gluten: '', dairy: '', picture: ''
+  });
+  const navigate = useNavigate();
+
+  const handleChange = e =>
+    setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    await addIngredient(form);
+    navigate('/recipes/:id');
+  };
+
+  return (
+    <>
+      <h1>Añadir ingrediente</h1>
+      <IngredientForm
+        form={form}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+    </>
+  );
+}
+
