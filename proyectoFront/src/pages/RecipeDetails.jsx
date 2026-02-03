eb040816931679e1dbc878d2067d3aa67ee0dc13
nayar <nayaragamer14@gmail.com>
2026-02-03 17:05:59 +0000

    Crear petición findRecipe

--- proyectoFront/src/pages/RecipeDetails.jsx
+++ proyectoFront/src/pages/RecipeDetails.jsx
@@ -1,61 +1,77 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { findRecipe } from '../services/recipeService';
 
 export default function Home() {
-    return (<>
-        <header>
-            <strong>CanaryFood</strong>
-            <button onClick={navigate('/recipes')}>Recetas</button>
-        </header>
-        <aside>
-            <img src="../assets/recipes/img/1_Tortitas_de_gofio.jpg" alt="Imagen de la receta" />
-            <ul class="lista">Ingredientes:
-                <li>
-                    <p>Nombre: <span>Gofio de millo</span> Tipo: <span>Otros</span></p>
-                    <p>Vegetariano: <span>Sí</span> Vegano: <span>Sí</span></p>
-                    <p>Azúcar: <span>No</span> Gluten: <span>No</span> Lácteo: <span>No</span></p>
-                    <img src="../assets/ingredients/img/1_Gofio_de_millo.jpg" alt="Imagen del ingrediente" />
-                </li>
-                <li>
-                    <p>Nombre: <span>Huevos</span> Tipo: <span>Otros</span></p>
-                    <p>Vegetariano: <span>Sí</span> Vegano: <span>No</span></p>
-                    <p>Azúcar: <span>No</span> Gluten: <span>No</span> Lácteo: <span>No</span></p>
-                    <img src="../assets/ingredients/img/2_Huevos.jpg" alt="Imagen del ingrediente" />
-                </li>
-            </ul>
-            <Link to={`/ingredients/add/${recipe.id}`}>
-                <button>Añadir ingrediente</button>
-            </Link>
-        </aside>
-        <main>
-            <h1>Detalles de la receta</h1>
-            <h2>Tortitas de gofio</h2>
-            <p>Tipo: <span>Desayuno</span></p>
-            <p>Autor: <span>Anónimo</span></p>
-            <p>Preparación:</p>
-            <p>Pasos</p>
-            <div class="acciones">
-                <Link to={`/recipes/update/${recipe.id}`}>
-                    <button>✏️</button>
+    const [recipe, setRecipe] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        loadRecipe();
+    }, []);
+
+    const loadRecipe = async () => {
+        try {
+            const data = await findRecipe();
+            setRecipe(data);
+        } catch (error) {
+            alert('No se pudieron cargar los datos');
+        } finally {
+            setLoading(false);
+        }
+        return (<>
+            <header>
+                <strong>CanaryFood</strong>
+                <button onClick={navigate('/recipes')}>Recetas</button>
+            </header>
+            <aside>
+                <img src={recipe.picture} alt="Imagen de la receta" />
+                <ul class="lista">Ingredientes:
+                    {recipe.ingredients.map((ingredient) => {
+                        <li>
+                            <p>Nombre: <span>{ingredient.name}</span> Tipo: <span>{ingredient.type}</span></p>
+                            <p>Vegetariano: <span>{ingredient.vegetarian}</span> Vegano: <span>{ingredient.vegan}</span></p>
+                            <p>Azúcar: <span>{ingredient.sugar}</span> Gluten: <span>{ingredient.gluten}</span> Lácteo: <span>{ingredient.dairy}</span></p>
+                            <img src={ingredient.picture} alt="Imagen del ingrediente" />
+                        </li>
+                    })}
+                </ul>
+                <Link to={`/ingredients/add/${recipe.id}`}>
+                    <button>Añadir ingrediente</button>
                 </Link>
-                <button onClick={() => confirmDelete(recipe)}>🗑️</button>
-                <Link to={'/recipes'}>
-                    <button>🔙</button>
+            </aside>
+            <main>
+                <h1>Detalles de la receta</h1>
+                <h2>{recipe.title}</h2>
+                <p>Tipo: <span>{recipe.type}</span></p>
+                <p>Autor: <span>Anónimo</span></p>
+                <p>Preparación:</p>
+                <p>{recipe.steps}</p>
+                <div class="acciones">
+                    <Link to={`/recipes/update/${recipe.id}`}>
+                        <button>✏️</button>
+                    </Link>
+                    <button onClick={() => confirmDelete(recipe)}>🗑️</button>
+                    <Link to={'/recipes'}>
+                        <button>🔙</button>
+                    </Link>
+                </div>
+            </main>
+            <aside>
+                <ul class="lista">Valoraciones:
+                    {recipe.reviews.map((review) => {
+                        <li>
+                            <p>Título: <span>{review.title}</span></p>
+                            <p>Descripción: <span>{review.description}</span></p>
+                            <p>{review.rating}</p>
+                            <p>Autor: <span>Anónimo</span></p>
+                        </li>
+                    })}
+                </ul>
+                <Link to={`/recipes/review/${recipe.id}`}>
+                    <button>Valorar</button>
                 </Link>
-            </div>
-        </main>
-        <aside>
-            <ul class="lista">Valoraciones:
-                <li>
-                    <p>Título: <span>Prueba1</span></p>
-                    <p>Descripción: <span>Me encanta</span></p>
-                    <p>٭٭٭٭٭</p>
-                    <p>Autor: <span>Anónimo</span></p>
-                </li>
-            </ul>
-            <Link to={`/recipes/review/${recipe.id}`}>
-                <button>Valorar</button>
-            </Link>
-        </aside>
-    </>)
+            </aside>
+        </>)
+    }
 }
